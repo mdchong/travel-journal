27672c415246095908671b30fdb385abbf0dc872
mdchong <mdchong@asu.edu>
2025-03-18 18:37:18 -0700

    .

--- App.jsx
+++ App.jsx
@@ -1,11 +1,27 @@
 import Header from "./Components/Header"
 import Entry from "./Components/Entry"
+import data from "./data"
 
 export default function App() {
+    const entryElements = data.map((entry) => {
+        return (
+            <Entry 
+                img={entry.img}
+                title={entry.title}
+                country={entry.country}
+                googleMapsLink={entry.googleMapsLink}
+                dates={entry.dates}
+                text={entry.text}
+            />
+        )
+    })
+
     return (
         <>
             <Header />
-            <Entry />
+            <main className="container">
+                {entryElements}
+            </main>
         </>
     )
 }
--- Components/Entry.jsx
+++ Components/Entry.jsx
@@ -1,24 +1,26 @@
-export default function Entry() {
+export default function Entry(props) {
     return (
         <main>
             <section className="card">
                 <div className="left-content">
-                    <img src="https://scrimba.com/links/travel-journal-japan-image-url" alt="" />
+                    <img 
+                        src={props.img.src} 
+                        alt={props.img.alt} />
                 </div>
 
                 <div className="right-content">
                     <div className="upper-content">
                         <img src="./images/marker.png" alt=""/>
-                        <p className="location">Japan</p>
-                        <a href="#" className="google-maps">View on Google Maps</a>
+                        <p className="location">{props.country}</p>
+                        <a href={props.googleMapsLink} className="google-maps">View on Google Maps</a>
                     </div>
 
-                    <h2 class="area">Mount Fuji</h2>
+                    <h2 class="area">{props.title}</h2>
 
-                    <p className="date">12 Jan, 2023–24 Jan, 2023</p>
+                    <p className="date">{props.date}</p>
 
                     <p className="description">
-                        Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.
+                        {props.text}
                     </p>
                 </div>
             </section>
